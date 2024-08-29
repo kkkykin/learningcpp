52d398bed60db171104cb17c613f540527b8e110
kkky <kkkykin@foxmail.com>
2024-08-29 17:26:20 +0800

    chore(): update

---
+++ 45/unsign-overflow0.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    unsigned short x{ 0 }; // smallest 2-byte unsigned value possible
+    std::cout << "x was: " << x << '\n';
+
+    x = -1; // -1 is out of our range, so we get modulo wrap-around
+    std::cout << "x is now: " << x << '\n';
+
+    x = -2; // -2 is out of our range, so we get modulo wrap-around
+    std::cout << "x is now: " << x << '\n';
+
+    return 0;
+}
